a15e707f0db7d1e90b9b5cc4e791ef12df5d0068
Gyana prkakash Khandual <gyanaprakashkhnadual@gmail.com>
2025-08-05 15:29:15 +0530

    Functional setup added

--- Functional/cypress.config.js
+++ Functional/cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
+      allureWriter(on, config);
+      return config;
     },
+    // Optional but recommended
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
   },
+  env: {
+    allure: true
+  }
 });
